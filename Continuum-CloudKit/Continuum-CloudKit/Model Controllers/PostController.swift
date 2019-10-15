0e6a2e4cf36e1324022ed65a9f28ec14d00f2d1f
Jordan Lamb <jdawg2329@gmail.com>
2019-10-15 14:06:57 -0600

    Post Model Controller Complete

---
+++ Continuum-CloudKit/Continuum-CloudKit/Model Controllers/PostController.swift
@@ -0,0 +1,29 @@
+//
+//  PostController.swift
+//  Continuum-CloudKit
+//
+//  Created by Jordan Lamb on 10/15/19.
+//  Copyright © 2019 Squanto Inc. All rights reserved.
+//
+
+import UIKit
+
+class PostController {
+    
+    // MARK: - Shared Instance - Singleton
+    static let shared = PostController()
+    
+    // MARK: - Source of Truth
+    var posts: [Post] = []
+    
+    // Add Comment
+    func addComment(text: String, post: Post, completion: @escaping (_ comment: Comment) -> Void) {
+        
+    }
+    
+    func createPostWith(image: UIImage, caption: String, completion: @escaping (_ post: Post) -> Void) {
+        let newPost = Post(photo: image, caption: caption)
+        posts.append(newPost)
+    }
+    
+}
