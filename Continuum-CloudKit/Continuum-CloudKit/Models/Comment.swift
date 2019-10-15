b91a4631472711c68a522ce123b1bce3ac058dd9
Jordan Lamb <jdawg2329@gmail.com>
2019-10-15 13:55:26 -0600

    Post Model and Comment Model Complete

---
+++ Continuum-CloudKit/Continuum-CloudKit/Models/Comment.swift
@@ -0,0 +1,21 @@
+//
+//  Comment.swift
+//  Continuum-CloudKit
+//
+//  Created by Jordan Lamb on 10/15/19.
+//  Copyright © 2019 Squanto Inc. All rights reserved.
+//
+
+import Foundation
+
+class Comment {
+    let text: String
+    let timestamp: Date
+    weak var post: Post?
+    
+    init(text: String, timestamp: Date = Date(), post: Post?) {
+        self.text = text
+        self.timestamp = timestamp
+        self.post = post
+    }
+}
