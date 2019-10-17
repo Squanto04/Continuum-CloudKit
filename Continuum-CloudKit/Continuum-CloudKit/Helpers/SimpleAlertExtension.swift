2f915fa42e3bc3df6947edf61206cf2834dfefc0
Jordan Lamb <jdawg2329@gmail.com>
2019-10-17 16:56:54 -0600

    Part 3 Complete

---
+++ Continuum-CloudKit/Continuum-CloudKit/Helpers/SimpleAlertExtension.swift
@@ -0,0 +1,9 @@
+//
+//  SimpleAlertExtension.swift
+//  Continuum-CloudKit
+//
+//  Created by Jordan Lamb on 10/17/19.
+//  Copyright © 2019 Squanto Inc. All rights reserved.
+//
+
+import Foundation
